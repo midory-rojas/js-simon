d84260c1ce09abe006ad46c1fb17d45a26c5bb1b
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-07 15:20:09 +0100

    feat:prendo tutti gli elementi attraverso javascript ed anche faccio console.log per vedere nel console si ha preso tutti

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// prendo tutti gli elementi
+
+const tempoElem = document.querySelector("h1");
+const istruzione = document.querySelector("p");
+const confermaBtn = document.querySelector("button");
+const risultati = document.querySelector(".risultati");
+const imput1= document.getElementById("num1");
+const imput2= document.getElementById("num2");
+const imput3= document.getElementById("num3");
+const imput4= document.getElementById("num4");
+const imput5= document.getElementById("num5");
+
+console.log(tempoElem, istruzione, confermaBtn, risultati, imput1, imput2, imput3, imput4, imput5);
+
+
